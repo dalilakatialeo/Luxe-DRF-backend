ef7d69c40a6617e46440479f6938b7b5e96e5fd7
Dalila K Leo <dalilakatialeo@gmail.com>
2021-11-23 20:06:34 +1000

    users

--- luxe/users/serializers.py
+++ luxe/users/serializers.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from rest_framework import serializers
 from .models import CustomUser
 
@@ -15,5 +16,33 @@ class UserDetailSerializer(CustomUserSerializer):
         instance.id = validated_data.get('id', instance.id)
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
+=======
+from django.contrib.auth import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = "__all__"
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = CustomUser(
+            email= validated_data["email"],
+            username= validated_data["username"]
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
+
+#update user details - name and email
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+>>>>>>> main
         instance.save()
         return instance
