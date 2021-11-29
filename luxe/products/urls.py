aba43d1cb0109dc16ca5ef5898d256b8be397769
iusharnina@gmail.com <zjulia679@gmail.com>
2021-11-29 15:28:46 +1000

    Added get, post, delete recommendations

--- luxe/products/urls.py
+++ luxe/products/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view()),
     path('products/<int:pk>', views.ProductDetail.as_view()),
+    path('recommendations/', views.RecommendationList.as_view()),
+    path('recommendations/<int:pk>/', views.RecommendationDetail.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- luxe/products/views.py
+++ luxe/products/views.py
@@ -3,8 +3,9 @@ from rest_framework import status, permissions
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer, ProductDetailSerializer
+from .models import Product, Recommendation
+from .serializers import ProductSerializer, ProductDetailSerializer, RecommendationSerializer, RecommendationDetailSerializer
+from .permissions import IsOwnerOrReadOnly
 
 #class Recommendation(APIVIew):
     #def post(self, request):
@@ -67,4 +68,49 @@ class ProductDetail(APIView):
     def delete(self, request, pk, format=None):
         product = self.get_object(pk)
         product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RecommendationList(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
+    def get(self, request):
+        recommendations = Recommendation.objects.all()
+        serializer = RecommendationSerializer(recommendations, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+            serializer = RecommendationSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(owner=request.user)
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED
+                    )
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+                )
+class RecommendationDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
+    def get_object(self, pk):
+        try:
+            recommendation = Recommendation.objects.get(pk=pk)
+            self.check_object_permissions(self.request, recommendation)
+            return recommendation
+        except Recommendation.DoesNotExist:
+            raise Http404
+    def get(self, request, pk):
+            recommendation = self.get_object(pk)
+            serializer = RecommendationDetailSerializer(recommendation)
+            return Response(serializer.data)    
+
+    def delete(self, request, pk, format=None):
+        recommendation = self.get_object(pk)
+        recommendation.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
