7a6dd63f71e0c0fbb42739c32ea99374ac6c246e
iusharnina@gmail.com <zjulia679@gmail.com>
2021-11-30 18:57:27 +1000

    Fixed users

--- luxe/users/serializers.py
+++ luxe/users/serializers.py
@@ -10,12 +10,13 @@ class CustomUserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = CustomUser(
-            email= validated_data["email"],
-            username= validated_data["username"]
-        )
-        user.set_password(validated_data["password"])
-        user.save()
+        # user = CustomUser(
+        #     email= validated_data["email"],
+        #     username= validated_data["username"]
+        # )
+        # user.set_password(validated_data["password"])
+        # user.save()
+        user = CustomUser.objects.create_user(**validated_data)
         return user
 
 #update user details - name and email
